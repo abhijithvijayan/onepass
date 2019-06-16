cd5b51ade07e641ddd3c161a3aead49fd478f432
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-16 14:10:43 +0530

    added scripts and updated package names

--- client/packages/web/config-overrides.js
+++ client/packages/web/config-overrides.js
@@ -16,6 +16,7 @@ module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(
     plugin => plugin.constructor.name !== 'ModuleScopePlugin'
   )
+
   config.module.rules[0].include = appIncludes
   config.module.rules[1] = null
   config.module.rules[2].oneOf[1].include = appIncludes
@@ -23,6 +24,7 @@ module.exports = function override(config, env) {
     require.resolve('babel-plugin-react-native-web'),
   ].concat(config.module.rules[2].oneOf[1].options.plugins)
   config.module.rules = config.module.rules.filter(Boolean)
+  
   config.plugins.push(
     new webpack.DefinePlugin({ __DEV__: env !== 'production' })
   )
