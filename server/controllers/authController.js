4a9d5ee1e86dab82443fbf42051d05a1ac4de867
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-24 12:19:26 +0530

    replace promise with async-await

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,18 +35,21 @@ const genToken = user =>
 );
 
 exports.signup = async (req, res) => {
-    const { email, password } = req.body;
-    if (password.length > 64) {
-        return res.status(400).json({ error: "Maximum length of Master Password is 64 characters"});
-    }
+    const { email, name } = req.body;
+    // if (password.length > 64) {
+    //     return res.status(400).json({ error: "Maximum length of Master Password is 64 characters"});
+    // }
     if (email.length > 64) {
         return res.status(400).json({ error: "Maximum length of email id is 64 characters"});
+    }
+    if (name.length > 64) {
+        return res.status(400).json({ error: "Maximum length of name is 64 characters"});
     }
     const user = await getUserDetails({ email });
     if (user && user.isVerified) {
         return res.status(403).json({ error: 'This email is already registered'});
     }
-    const newUser = await createUser({ email, password });
+    const newUser = await createUser({ email, name });
     /* Handle Verification email */
     const text = verifyMailText.replace(/{{domain}}/gim, process.env.DEFAULT_DOMAIN);
     const mail = await transporter.sendMail({
--- server/db/user.js
+++ server/db/user.js
@@ -1,142 +1,111 @@
 const nanoid = require('nanoid');
+const generate = require('nanoid/generate');
 const bcrypt = require('bcryptjs');
 
 const driver = require('./neo4j');
 
-exports.createUser = ({ email, password }) => {
-    return new Promise(async (resolve, reject) => {
-        const session = driver.session();
-        const salt = await bcrypt.genSalt(12);
-        const passwordHash = await bcrypt.hash(password, salt);
-        const verificationToken = nanoid(40);
-        session
-            .writeTransaction(tx =>
-                tx.run(
-                    'MERGE (id:UniqueId { identifier: $identifierParam, userPrefix: $userPrefixParam }) ' + 
-                    'ON CREATE SET id.count = 1 ' +
-                    'ON MATCH SET id.count = id.count + 1 ' +
-                    'WITH id.userPrefix + id.count AS uid, id ' +  
-                    'MERGE (u:User { email : $emailParam }) ' +
-                    'ON CREATE SET u.uid = uid, u.password = $passwordParam, u.verificationToken = $verificationTokenParam, u.isVerified = $isVerifiedParam, u.createdAt = $createdAtParam ' + 
-                    'ON MATCH SET id.count = id.count - 1, u.password = $passwordParam, u.verificationToken = $verificationTokenParam, u.isVerified = $isVerifiedParam, u.createdAt = $createdAtParam ' + 
-                    'RETURN u',
-                    { 
-                        identifierParam: 'UserCounter',
-                        userPrefixParam: 'user_',
-                        emailParam: email, 
-                        passwordParam: passwordHash, 
-                        verificationTokenParam: verificationToken, 
-                        isVerifiedParam: false, 
-                        createdAtParam: new Date().toJSON() 
-                    }
-                )
-            )
-            .then(function (res) {
-                session.close();
-                const user = res.records.length && res.records[0].get('u').properties;                                   
-                // console.log(user);
-                return resolve(user);
-            })
-            .catch(err => reject(err));
-    })
+exports.createUser = async ({ email, name }) => {
+    const session = driver.session();
+    const uuid = generate('1234567890abcdefghijklmnopqrstuvwxyz', 5)
+    const verificationToken = generate('1234567890', 6);
+    const { records = [] } = await session.writeTransaction(tx =>
+        tx.run(
+            'MERGE (id:UniqueId { identifier: $identifierParam, userPrefix: $userPrefixParam }) ' + 
+            'ON CREATE SET id.count = 1 ' +
+            'ON MATCH SET id.count = id.count + 1 ' +
+            'WITH id.userPrefix + id.count AS uid, id ' +  
+            'MERGE (u:User { email : $emailParam }) ' +
+            'ON CREATE SET u.uid = uid, u.name = $nameParam, u.verificationToken = $verificationTokenParam, u.isVerified = $isVerifiedParam, u.createdAt = $createdAtParam ' + 
+            'ON MATCH SET id.count = id.count - 1, u.name = $nameParam, u.verificationToken = $verificationTokenParam, u.isVerified = $isVerifiedParam, u.createdAt = $createdAtParam ' + 
+            'RETURN u',
+            { 
+                identifierParam: 'UserCounter',
+                userPrefixParam: 'user_',
+                emailParam: email, 
+                nameParam: name, 
+                verificationTokenParam: verificationToken, 
+                isVerifiedParam: false, 
+                createdAtParam: new Date().toJSON() 
+            }
+        )
+    )
+    session.close();
+    const user = records[0].get('u').properties;
+    return user;
 };
 
-exports.getUserDetails = ({ email }) => {
-    return new Promise((resolve, reject) => {
-        const session = driver.session();
-        session
-            .readTransaction(tx => 
-                tx.run(
-                    'MATCH (u:User { email : $emailParam }) ' + 
-                    'RETURN u', 
-                    { 
-                        emailParam: email 
-                    }
-                )    
-            )
-            .then(function (res) {
-                session.close();
-                const user = res.records.length && res.records[0].get('u').properties;
-                return resolve(user);
-            })
-            .catch(err => reject(err));
-    })
+exports.getUserDetails = async ({ email }) => {
+    const session = driver.session();
+    const { records = [] } = await session.readTransaction(tx =>
+        tx.run(
+            'MATCH (u:User { email : $emailParam }) ' + 
+            'RETURN u', 
+            { 
+                emailParam: email 
+            }
+        )    
+    );
+    session.close();
+    const user = records.length && records[0].get('u').properties;
+    return user;
 };
 
-exports.verifyUser = ({ email, verificationToken }) => {
-    return new Promise((resolve, reject) => {
-        const session = driver.session();
-        session
-            .writeTransaction(tx => 
-                tx.run(
-                    'MATCH (u:User { email: $emailParam, verificationToken: $verificationTokenParam }) ' + 
-                    'SET u.isVerified = true, u.verificationToken = NULL ' + 
-                    'RETURN u', 
-                    { 
-                        emailParam: email, 
-                        verificationTokenParam: verificationToken 
-                    }
-                )
-            )
-            .then(function (res) {
-                session.close();
-                const user = res.records.length && res.records[0].get('u').properties;
-                return resolve(user);
-            })
-            .catch(err => reject(err));
-    })
+exports.verifyUser = async ({ email, verificationToken }) => {
+    const session = driver.session();
+    const { records = [] } = await session.writeTransaction(tx =>
+        tx.run(
+            'MATCH (u:User { email: $emailParam, verificationToken: $verificationTokenParam }) ' + 
+            'SET u.isVerified = true, u.verificationToken = NULL ' + 
+            'RETURN u', 
+            { 
+                emailParam: email, 
+                verificationTokenParam: verificationToken 
+            }
+        )
+    );
+    session.close();
+    const user = records.length && records[0].get('u').properties;
+    return user;
 };
 
-exports.requestResetPassword = ({ email }) => {
-    return new Promise((resolve, reject) => {
-        const session = driver.session();
-        const passwordResetToken = nanoid(40);
-        const passwordResetExpires = Date.now() + 3600000;
-        session
-            .writeTransaction(tx =>
-                tx.run(
-                    'MATCH (u:User { email: $emailParam }) ' +
-                    'SET u.passwordResetToken = $passwordResetTokenParam, u.passwordResetExpires = $passwordResetExpiresParam ' + 
-                    'RETURN u', 
-                    { 
-                        emailParam: email, 
-                        passwordResetTokenParam : passwordResetToken,
-                         passwordResetExpiresParam: passwordResetExpires
-                    }
-                )    
-            )
-            .then(function (res) {
-                session.close();
-                const user = res.records.length && res.records[0].get('u').properties;
-                return resolve(user);
-            })
-            .catch(err => reject(err));
-    })
+exports.requestResetPassword = async ({ email }) => {
+    const session = driver.session();
+    const passwordResetToken = nanoid(40);
+    const passwordResetExpires = Date.now() + 3600000;
+    const { records = [] } = await session.writeTransaction(tx =>
+        tx.run(
+            'MATCH (u:User { email: $emailParam }) ' +
+            'SET u.passwordResetToken = $passwordResetTokenParam, u.passwordResetExpires = $passwordResetExpiresParam ' + 
+            'RETURN u', 
+            { 
+                emailParam: email, 
+                passwordResetTokenParam : passwordResetToken,
+                    passwordResetExpiresParam: passwordResetExpires
+            }
+        )    
+    );
+    session.close();
+    const user = records.length && records[0].get('u').properties;
+    return user;
 };
 
-exports.validatePasswordRequest = ({ email, passwordResetToken }) => {
-    return new Promise((resolve, reject) => {
-        const session = driver.session();
-        session
-            .writeTransaction(tx =>
-                tx.run(
-                    'MATCH (u:User) ' +
-                    'WHERE u.email = $emailParam AND u.passwordResetToken = $passwordResetTokenParam AND u.passwordResetExpires > $currentTime ' +
-                    'SET u.passwordResetToken = NULL, u.passwordResetExpires = NULL ' +
-                    'RETURN u', 
-                    { 
-                        emailParam: email, 
-                        passwordResetTokenParam: passwordResetToken, 
-                        currentTime: Date.now() 
-                    }
-                )
-            )
-            .then(function (res) {
-                session.close();
-                const user = res.records.length && res.records[0].get('u').properties;
-                console.log(user);
-                return resolve(user);
-            })
-            .catch(err => reject(err));
-    })
+exports.validatePasswordRequest = async ({ email, passwordResetToken }) => {
+    const session = driver.session();
+    const { records = [] } = await session.writeTransaction(tx =>
+        tx.run(
+            'MATCH (u:User) ' +
+            'WHERE u.email = $emailParam AND u.passwordResetToken = $passwordResetTokenParam AND u.passwordResetExpires > $currentTime ' +
+            'SET u.passwordResetToken = NULL, u.passwordResetExpires = NULL ' +
+            'RETURN u', 
+            { 
+                emailParam: email, 
+                passwordResetTokenParam: passwordResetToken, 
+                currentTime: Date.now() 
+            }
+        )
+    );
+    session.close();
+    const user = records.length && records[0].get('u').properties;
+    console.log(user);
+    return user;
 };
