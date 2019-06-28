0445020067310c9f6c0bbf7b57ba1ba254ed16d2
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-28 22:31:57 +0530

    add next-compose-plugins for env variables config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,11 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withTM = require('next-transpile-modules');
+const { parsed: localEnv } = require('dotenv').config({ path: 'variables.env' });
+const withPlugins = require('next-compose-plugins');
 
-// Tell webpack to compile the "@onepass/core" package
-// https://www.npmjs.com/package/next-transpile-modules
-module.exports = withTM({
-    transpileModules: ['@onepass/core'],
-});
+module.exports = withPlugins(
+    [
+        [
+            withTM,
+            {
+                // Tell webpack to compile the "@onepass/core" package
+                transpileModules: ['@onepass/core', '@onepass/server'],
+            },
+        ],
+        withCSS,
+        withSass,
+    ],
+    {
+        webpack(config) {
+            config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
-module.exports = withSass(withCSS());
+            return config;
+        },
+    }
+);
