298925cf5a98f2ec615a30c65d28eb787a8d7a4d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-28 10:53:40 +0530

    fix/redux-form bring formReducer to rootReducer root level

--- packages/web/state/modules/common/form/reducer.js
+++ packages/web/state/modules/common/form/reducer.js
@@ -1,6 +1,3 @@
 import { reducer as formReducer } from 'redux-form';
-import { combineReducers } from 'redux';
 
-export default combineReducers({
-    forms: formReducer,
-});
+export default formReducer;
--- packages/web/state/modules/index.js
+++ packages/web/state/modules/index.js
@@ -1,3 +1,3 @@
 export { default as vault } from './vault';
 export { default as auth } from './auth';
-export { default as common } from './common/form';
+export { default as form } from './common/form';
