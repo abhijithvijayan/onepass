5f74d7827f16bd112502c0a81ba42d50ccc706aa
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-06-29 20:36:53 +0530

    add missing common packages

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const webpack = require('webpack');
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
