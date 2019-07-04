26e0325d109b8afebef9abefb42bd2038f3ddedb
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-04 21:15:35 +0530

    add antd packages, test page & setup config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -21,9 +21,30 @@ module.exports = withPlugins(
         withImages,
     ],
     {
-        webpack(config) {
+        webpack(config, { isServer }) {
+            // local env variables
             config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+            // antdesign css config
+            if (isServer) {
+                const antStyles = /antd\/.*?\/style\/css.*?/;
+                const origExternals = [...config.externals];
+                config.externals = [
+                    (context, request, callback) => {
+                        if (request.match(antStyles)) return callback();
+                        if (typeof origExternals[0] === 'function') {
+                            origExternals[0](context, request, callback);
+                        } else {
+                            callback();
+                        }
+                    },
+                    ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+                ];
 
+                config.module.rules.unshift({
+                    test: antStyles,
+                    use: 'null-loader',
+                });
+            }
             return config;
         },
     }
