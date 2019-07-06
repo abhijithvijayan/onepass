91f62960aa5103b26c404ef0570e7bd1368d1dc8
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-06 15:08:44 +0530

    move srp computation functions to @onepass/core/srp/

--- packages/core/srp/index.js
+++ packages/core/srp/index.js
@@ -1,5 +1,5 @@
-import { computeVerifier } from './signup/computeVerifier';
-import { genClientEphemeral } from './login/genClientEphemeral';
-import { deriveClientSession, verifyLoginSession } from './login/deriveClientSession';
+import { computeVerifier } from './computeVerifier';
+import { genClientEphemeral } from './genClientEphemeral';
+import { deriveClientSession, verifyLoginSession } from './deriveClientSession';
 
 export { computeVerifier, genClientEphemeral, deriveClientSession, verifyLoginSession };
