07087eb3155b94976448a5ff06ee8e0480f34786
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-07 22:22:47 +0530

    modify masterPasswordForm input with react-password-strength & custom antd config

--- packages/web/components/SignUp/MasterPasswordForm.js
+++ packages/web/components/SignUp/MasterPasswordForm.js
@@ -4,13 +4,41 @@ import { bindActionCreators } from 'redux';
 import { Field, reduxForm } from 'redux-form';
 import { Button, Form, Icon, Input } from 'antd';
 
+import ReactPasswordStrength from 'react-password-strength/dist/universal';
+import styled from 'styled-components';
+
 import { completeSignUp } from '../../state/modules/auth/operations';
 
-const renderInputField = ({ input, type, icon, label, meta: { touched, invalid, error } }) => {
+const renderInputField = ({ input, icon, label, meta: { touched, invalid, error } }) => {
     const isInvalid = touched && invalid;
     return (
         <Form.Item label={label} validateStatus={isInvalid ? 'error' : 'success'} help={isInvalid && error}>
-            <Input {...input} type={type} prefix={<Icon type={icon} />} />
+            <div className="d-flex">
+                <span className="ant-input-prefix custom-ant-lock">
+                    <i aria-label="icon: lock" className="anticon anticon-lock">
+                        <svg
+                            viewBox="64 64 896 896"
+                            focusable="false"
+                            className=""
+                            data-icon="lock"
+                            width="1em"
+                            height="1em"
+                            fill="currentColor"
+                            aria-hidden="true"
+                        >
+                            <path d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 1 0-56 0z"></path>
+                        </svg>
+                    </i>
+                </span>
+                <ReactPasswordStrength
+                    inputProps={{ ...input }}
+                    prefix={<Icon type={icon} />}
+                    className="w-100 password__strength--input"
+                    minLength={10}
+                    minScore={4}
+                    scoreWords={['weak', 'okay', 'good', 'strong', 'stronger']}
+                />
+            </div>
         </Form.Item>
     );
 };
@@ -25,20 +53,8 @@ class MasterPasswordForm extends Component {
         const { handleSubmit } = this.props;
         return (
             <Form onSubmit={handleSubmit(this.onSubmit)}>
-                <Field
-                    name="password"
-                    type="password"
-                    icon="lock"
-                    component={renderInputField}
-                    label="Enter a password"
-                />
-                <Field
-                    name="confirmpassword"
-                    type="password"
-                    icon="lock"
-                    component={renderInputField}
-                    label="Confirm your password"
-                />
+                <Field name="password" icon="lock" component={renderInputField} label="Enter a password" />
+                <Field name="confirmpassword" icon="lock" component={renderInputField} label="Confirm your password" />
                 <Button type="primary" htmlType="submit">
                     Create OnePass account
                 </Button>
