a4d3eb84d0bcf94c7ed911025693063c53285f7f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-07 20:35:36 +0530

    add nprogress to @onepass/web

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -5,12 +5,24 @@ import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
 import withRedux from 'next-redux-wrapper';
 import { ThemeProvider } from 'styled-components';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 import { initializeStore } from '../state/store';
 
 /* Common SASS styles */
 import 'normalize.css/normalize.css';
 import '../styles/main.scss';
+
+Router.events.on('routeChangeStart', url => {
+    NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+    return NProgress.done();
+});
+Router.events.on('routeChangeError', () => {
+    return NProgress.done();
+});
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!../styles/base/_variables.scss');
