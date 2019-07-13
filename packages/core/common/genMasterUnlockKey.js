f2d2d91dd0dcb61eefb7bf278f794c13466ab5dd
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-13 11:57:36 +0530

    remove `bitwise` and perform xor with function

--- packages/core/common/genMasterUnlockKey.js
+++ packages/core/common/genMasterUnlockKey.js
@@ -1,14 +1,26 @@
-import bitwise from 'bitwise';
+/* eslint-disable no-bitwise */
 
 /**
- * Generate MUK
+ * Outputs XOR operation between two arrays
+ * @param {Array, Array}
+ */
+
+function xorVectors(a, b) {
+    const length = Math.min(a.length, b.length);
+    const res = new Array(length);
+    for (let i = 0; i < length; i += 1) {
+        res[i] = a[i] ^ b[i];
+    }
+    return res;
+}
+
+/**
+ * Generate Master Unlock Key
  * @param {Object}
  */
 
 export const genMasterUnlockKey = ({ hashedKey, intermediateKey }) => {
-    // XOR Operation
-    const XORedKey = bitwise.bits.xor(hashedKey, intermediateKey);
-    // To Uint8Array
-    const masterUnlockKey = new Uint8Array(XORedKey);
+    // XOR Operation Output To Uint8Array
+    const masterUnlockKey = new Uint8Array(xorVectors(hashedKey, intermediateKey));
     return masterUnlockKey;
 };
