a32ca1ddf269ec2063f3736f86e5ddaba4076f3e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-17 11:37:53 +0530

    create pdf content

---
+++ packages/web/components/Vault/PdfContent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PdfContent = props => {
+    const today = new Date();
+    const date = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+    return (
+        <div>
+            <h2>OnePass Emergency Kit</h2>
+            <p style={{ textAlign: 'center' }}>Generated on {date}</p>
+            <br />
+            <p>The secret key is to be used with master password on login.</p>
+            <ul>
+                <li>1. Print out this document (and/or put it on a USB key or external drive).</li>
+                <li>2. Store your kit in a secure place where you can find it, e.g. a safe deposit box.</li>
+            </ul>
+            <div>
+                <ul>
+                    <li>
+                        EMAIL ADDRESS <br /> {props.email}
+                    </li>
+                    <li>
+                        SECRET KEY <br /> {props.secretKey}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default PdfContent;
