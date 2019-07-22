9d8431318e54a90c29fc3f320b371a2619ac12dc
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-22 09:13:00 +0530

    set dir structure for extension like web

--- packages/extension/src/index.js
+++ packages/extension/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './App';
+import App from './pages/_app';
 import { initializeStore } from './state/store';
 
 const store = initializeStore(window.REDUX_INITIAL_DATA);
--- packages/extension/src/pages/_app.js
+++ packages/extension/src/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import Home from '@onepass/web/components/Home';
-import history from './state/utils/history';
+import history from '../state/utils/index';
 
 const App = () => {
     return (
