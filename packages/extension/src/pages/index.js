f7266bd5c45a012f194ce4ae94221ae45512bf48
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-22 20:17:45 +0530

    add webextensions-polyfill & setup config

--- packages/extension/src/pages/index.js
+++ packages/extension/src/pages/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import browser from 'webextension-polyfill';
 
 import Home from '@onepass/web/components/Home';
 import BodyWrapper from '@onepass/web/components/BodyWrapper';
 
 class IndexPage extends Component {
+    componentDidMount() {
+        /* eslint-disable no-undef */
+        browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+            browser.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, function(response) {
+                /* eslint-disable no-console */
+                console.log(response);
+            });
+        });
+    }
+
     render() {
         return (
             <BodyWrapper>
--- packages/extension/src/scripts/background.js
+++ packages/extension/src/scripts/background.js
@@ -1,4 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-    /* eslint-disable-next-line no-console */
+/* eslint-disable no-console */
+import browser from 'webextension-polyfill';
+
+browser.runtime.onInstalled.addListener(() => {
     console.log('onInstalled....');
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // Do something with the message!
+    alert(request.greeting);
+
+    // And respond back to the sender.
+    sendResponse('got your message, thanks!');
 });
