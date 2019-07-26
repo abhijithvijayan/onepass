b5f88535c1dad92f90091cc6809d03939a3d9589
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-26 09:05:44 +0530

    add server side body validation for enc credentials

--- server/controllers/validateBodyController.js
+++ server/controllers/validateBodyController.js
@@ -54,7 +54,7 @@ exports.emailVerificationCriterias = [
         .exists()
         .withMessage('No valid verification token.')
         .matches(/^[0-9]+$/)
-        .withMessage('Verification token must be a 6-digit number.')
+        .withMessage('Verification token must be a number.')
         .isLength({ min: 6, max: 6 })
         .withMessage('Verification token must be 6 characters long.'),
 ];
@@ -89,21 +89,93 @@ exports.finalizeAccountValidationCriterias = [
     validator
         .body('userId')
         .exists()
-        .withMessage('No valid userId.')
+        .withMessage('No valid userId found.')
         .isLength({ min: 13 })
         .withMessage('User id must be 13 characters or more.'),
     validator
         .body('salt')
         .exists()
-        .withMessage('No valid salt.'),
+        .withMessage('No valid salt found.'),
     validator
         .body('verifier')
         .exists()
-        .withMessage('No valid verifier.'),
+        .withMessage('No valid verifier found.'),
     validator
         .body('encryptionKeys')
         .exists()
-        .withMessage('No valid encryption keys.'),
+        .withMessage('No valid encryption keys found.'),
+    validator
+        .body('encryptionKeys.pubKey')
+        .exists()
+        .withMessage('No valid encrypted public key param found.'),
+    validator
+        .body('encryptionKeys.pubKey.key')
+        .exists()
+        .withMessage('No valid encrypted public key found.'),
+    validator
+        .body('encryptionKeys.encPriKey')
+        .exists()
+        .withMessage('No valid encrypted private key param found.'),
+    validator
+        .body('encryptionKeys.encPriKey.enc')
+        .exists()
+        .withMessage('No valid encrypted private key found.'),
+    validator
+        .body('encryptionKeys.encPriKey.key')
+        .exists()
+        .withMessage('No valid encrypted private key found.'),
+    validator
+        .body('encryptionKeys.encSymKey')
+        .exists()
+        .withMessage('No valid encrypted symmetric key param found.'),
+    validator
+        .body('encryptionKeys.encSymKey.kid')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.enc')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.tagLength')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.tag')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.key')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.iv')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.iterations')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encSymKey.salt')
+        .exists()
+        .withMessage('No valid encrypted symmetric key found.'),
+    validator
+        .body('encryptionKeys.encVaultKey')
+        .exists()
+        .withMessage('No valid encrypted vault key param found.'),
+    validator
+        .body('encryptionKeys.encVaultKey.alg')
+        .exists()
+        .withMessage('No valid encrypted vault key found.'),
+    validator
+        .body('encryptionKeys.encVaultKey.kty')
+        .exists()
+        .withMessage('No valid encrypted vault key found.'),
+    validator
+        .body('encryptionKeys.encVaultKey.key')
+        .exists()
+        .withMessage('No valid encrypted vault key found.'),
 ];
 
 exports.finalizeAccountValidationBody = (req, res, next) => {
@@ -115,9 +187,72 @@ exports.finalizeAccountValidationBody = (req, res, next) => {
         const saltError = errorsObj.salt && errorsObj.salt.msg;
         const verifierError = errorsObj.verifier && errorsObj.verifier.msg;
         const encryptionKeysError = errorsObj.encryptionKeys && errorsObj.encryptionKeys.msg;
+
+        // EncPubKey
+        const pubKeyError = errorsObj['encryptionKeys.pubKey'] && errorsObj['encryptionKeys.pubKey'].msg;
+        const pubKeyBodyKeyError = errorsObj['encryptionKeys.pubKey.key'] && errorsObj['encryptionKeys.pubKey.key'].msg;
+
+        // EncPriKey
+        const priKeyError = errorsObj['encryptionKeys.encPriKey'] && errorsObj['encryptionKeys.encPriKey'].msg;
+        const priKeyBodyEncError =
+            errorsObj['encryptionKeys.encPriKey.enc'] && errorsObj['encryptionKeys.encPriKey.enc'].msg;
+        const priKeyBodyKeyError =
+            errorsObj['encryptionKeys.encPriKey.key'] && errorsObj['encryptionKeys.encPriKey.key'].msg;
+
+        // EncSymKey
+        const symKeyError = errorsObj['encryptionKeys.encSymKey'] && errorsObj['encryptionKeys.encSymKey'].msg;
+        const symKeyBodyKidError =
+            errorsObj['encryptionKeys.encSymKey.kid'] && errorsObj['encryptionKeys.encSymKey.kid'].msg;
+        const symKeyBodyEncError =
+            errorsObj['encryptionKeys.encSymKey.enc'] && errorsObj['encryptionKeys.encSymKey.enc'].msg;
+        const symKeyBodyTagLengthError =
+            errorsObj['encryptionKeys.encSymKey.tagLength'] && errorsObj['encryptionKeys.encSymKey.tagLength'].msg;
+        const symKeyBodyTagError =
+            errorsObj['encryptionKeys.encSymKey.tag'] && errorsObj['encryptionKeys.encSymKey.tag'].msg;
+        const symKeyBodyKeyError =
+            errorsObj['encryptionKeys.encSymKey.key'] && errorsObj['encryptionKeys.encSymKey.key'].msg;
+        const symKeyBodyIVError =
+            errorsObj['encryptionKeys.encSymKey.iv'] && errorsObj['encryptionKeys.encSymKey.iv'].msg;
+        const symKeyBodyIterationsError =
+            errorsObj['encryptionKeys.encSymKey.iterations'] && errorsObj['encryptionKeys.encSymKey.iterations'].msg;
+        const symKeyBodySaltError =
+            errorsObj['encryptionKeys.encSymKey.salt'] && errorsObj['encryptionKeys.encSymKey.salt'].msg;
+
+        // EncVaultKey
+        const vaultKeyError = errorsObj['encryptionKeys.encVaultKey'] && errorsObj['encryptionKeys.encVaultKey'].msg;
+        const vaultKeyBodyAlgError =
+            errorsObj['encryptionKeys.encVaultKey.alg'] && errorsObj['encryptionKeys.encVaultKey.alg'].msg;
+        const vaultKeyBodyKtyError =
+            errorsObj['encryptionKeys.encVaultKey.kty'] && errorsObj['encryptionKeys.encVaultKey.kty'].msg;
+        const vaultKeyBodyKeyError =
+            errorsObj['encryptionKeys.encVaultKey.key'] && errorsObj['encryptionKeys.encVaultKey.key'].msg;
+
         return res.status(400).json({
             error: {
-                msg: emailError || userIdError || saltError || verifierError || encryptionKeysError,
+                msg:
+                    emailError ||
+                    userIdError ||
+                    saltError ||
+                    verifierError ||
+                    encryptionKeysError ||
+                    pubKeyError ||
+                    pubKeyBodyKeyError ||
+                    priKeyError ||
+                    priKeyBodyEncError ||
+                    priKeyBodyKeyError ||
+                    symKeyError ||
+                    symKeyBodyKidError ||
+                    symKeyBodyEncError ||
+                    symKeyBodyTagLengthError ||
+                    symKeyBodyTagError ||
+                    symKeyBodyKeyError ||
+                    symKeyBodyIVError ||
+                    symKeyBodyIterationsError ||
+                    symKeyBodySaltError ||
+                    vaultKeyError ||
+                    vaultKeyBodyAlgError ||
+                    vaultKeyBodyKtyError ||
+                    vaultKeyBodyKeyError,
                 reportedAt: new Date().getTime(),
             },
         });
@@ -178,7 +313,7 @@ exports.addOrUpdateItemCriterias = [
     validator
         .body('encDetails.tagLength')
         .exists()
-        .withMessage('Missing Encrypted Details Tag Length Field.'),
+        .withMessage('Missing Encrypted Details Tag-Length Field.'),
     validator
         .body('encDetails.iv')
         .exists()
@@ -198,7 +333,7 @@ exports.addOrUpdateItemCriterias = [
     validator
         .body('encOverview.tagLength')
         .exists()
-        .withMessage('Missing Encrypted Overview Tag Length Field.'),
+        .withMessage('Missing Encrypted Overview Tag-Length Field.'),
     validator
         .body('encOverview.iv')
         .exists()
@@ -217,12 +352,15 @@ exports.addOrUpdateItemBody = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const errorsObj = errors.mapped();
+
+        // EncDetails
         const encDetailsError = errorsObj.encDetails && errorsObj.encDetails.msg;
         const encDetailsTagError = errorsObj['encDetails.tag'] && errorsObj['encDetails.tag'].msg;
         const encDetailsDataError = errorsObj['encDetails.data'] && errorsObj['encDetails.data'].msg;
         const encDetailsTagLengthError = errorsObj['encDetails.tagLength'] && errorsObj['encDetails.tagLength'].msg;
         const encDetailsIVError = errorsObj['encDetails.iv'] && errorsObj['encDetails.iv'].msg;
 
+        // EncOverview
         const encOverviewError = errorsObj.encOverview && errorsObj.encOverview.msg;
         const encOverviewTagError = errorsObj['encOverview.tag'] && errorsObj['encOverview.tag'].msg;
         const encOverviewDataError = errorsObj['encOverview.data'] && errorsObj['encOverview.data'].msg;
